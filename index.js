09ed985cc6cbe902d51be9fcf666e91d145ffdea
Olly Britton <ollybritton@gmail.com>
2022-01-23 16:51:24 +0000

    add metadata view

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+let Graph, data;
+
 // loadYAML returns the data in the specs.yaml file, parsed into an object by js-yaml.
 function loadYAML(filename) {
     return new Promise(resolve => {
@@ -86,22 +88,194 @@ function calculateImportance(node) {
     return base
 }
 
+// standardiseText adds full stops to the end of description text and replaces quotes with typographical ones if neccessary.
+function standardiseText(text) {
+    if (!(text.endsWith(".") || text.endsWith("!") || text.endsWith("?"))) {
+        text += "."
+    }
+
+    return text
+}
+
+// createInformation returns the HTML displaying information about a specific node.
+function createInformation(node) {
+    let headingLevel = 0;
+    switch (node.type) {
+        case "$subject":
+            headingLevel = 1;
+            break;
+        case "$module":
+            headingLevel = 2;
+            break;
+        case "$topic":
+            headingLevel = 3;
+            break;
+        case "$subtopic":
+            headingLevel = 4;
+            break;
+    }
+
+    var output = "";
+
+    output += `<h${headingLevel} class="node-name">${node.name}</h${headingLevel}>`
+    
+    
+    let {parent, children, relates} = getLinks(node.id)
+    
+    if (parent != null) {
+        output += `<i>Parent: <a href="javascript:void(0);" data-link-to="${parent.id}">${parent.name}</a></i>`
+    }
+    
+    if (node.description != undefined) {
+        output += `<p class="node-description">${standardiseText(node.description)}</p>`
+    }
+    
+    if (node.examples != undefined && node.examples.length > 0) {
+        output += `<hr />`
+        output += `<h6>Examples</h6>`
+
+        for (let example of node.examples) {
+            output += `<blockquote>${example}</blockquote>`
+        } 
+    }
+
+    if (children.length > 0 || relates.length > 0) {
+        output += `<hr />`
+        output += `<div class="links">`
+        output += `<div class="children">`
+        output += `<h6 class="node-info-heading">Children</h6 class="node-info-heading">`
+        
+        if (children.length == 0) {
+            output += `<i>This topic has no children.</i>`
+        } else {
+            output += "<ul>"
+            
+            for (let child of children) {
+                output += `<li class="child-link"><a href="javascript:void(0);" data-link-to="${child.id}">${child.name}</a></li>`    
+            }
+            
+            output += "</ul>"
+        }
+        
+        output += `</div>`
+        output += `<div class="cross-topic">`
+        output += `<h6 class="node-info-heading">Cross-topic links</h6 class="node-info-heading">`
+
+        if (relates.length == 0) {
+            output += `<i>This topic has no identified cross-topic links.</i>`
+        } else {
+            output += "<ul>"
+            
+            for (let linkedNode of relates) {
+                output += `<li class="relates-link"><a href="javascript:void(0);" data-link-to="${linkedNode.id}">${linkedNode.name}</a></li>`    
+            }
+
+            output += "</ul>"
+        }
+
+        output += `</div>`
+        output += `</div>`
+    }
+
+    
+
+    console.log(getLinks(node.id))
+
+    return output
+}
+
+// getNodeByID gets a node by the ID from the graph data.
+function getNodeByID(id, data) {
+    for (let node of data.nodes) {
+        if (node.id == id) {
+            return node
+        }
+    }
+}
+
+// getRelatedNodes gets the parent of this node if it exists and the list of nodes associated with it.
+function getLinks(id) {
+    let links = {
+        parent: null,
+        children: [],
+        relates: []
+    };
+
+    for (let link of data.links) {
+        if (link.type == "child") {
+            if (link.target.id == id) {
+                links.parent = link.source
+            } else if (link.source.id == id) {
+                links.children.push(link.target)
+            }
+        } else if (link.type == "relates") {
+            if (link.source.id == id) {
+                links.relates.push(link.target)
+            } else if (link.target.id == id) {
+                links.relates.push(link.source)
+            }
+        }
+    }
+
+    return links
+}
+
+// handleLink makes it so that links will zoom the graph.
+function handleLink(e) {
+    alert(e)
+}
+
 loadYAML("specs.yaml").then(specData => {
-    let data = graphFromYAML(specData)
+    data = graphFromYAML(specData)
     console.log(data)
 
     const elem = document.getElementById('graph');
+    const info = document.getElementById('info');
 
-    const Graph = ForceGraph()(elem)
+    Graph = ForceGraph()(elem)
       .graphData(data)
       .nodeLabel('name')
       .nodeAutoColorBy('type')
       .nodeVal(calculateImportance)
       .linkDirectionalParticles(2)
       .linkDirectionalParticleWidth(1.4)
-      .onNodeClick(node => {
-        // Center/zoom on node
-        Graph.centerAt(node.x, node.y, 500);
-        Graph.zoom(8, 1000);
-      });
+      .onNodeClick(handleNodeClick);
 })
+
+function handleNodeClick(node) {
+    if (Graph.centerAt == undefined) {
+        const distance = 40;
+        const distRatio = 1 + distance/Math.hypot(node.x, node.y, node.z);
+
+        Graph.cameraPosition(
+            { x: node.x * distRatio, y: node.y * distRatio, z: node.z * distRatio }, // new position
+            node, // lookAt ({ x, y, z })
+            1000  // ms transition duration
+        );
+    } else {
+        Graph.centerAt(node.x, node.y, 500)
+        Graph.zoom(8, 1000)
+    }
+
+    info.innerHTML = createInformation(node);
+    MathJax.typeset()
+}
+
+function interceptClickEvent(e) {
+    var target = e.target || e.srcElement;
+    if (target.tagName === 'A') {
+        var linkTo = target.getAttribute('data-link-to');
+        var href = target.getAttribute("href")
+        
+        if (href != "javascript:void(0);") {
+            return
+        }
+        
+        e.preventDefault();
+        node = getNodeByID(linkTo, data)
+
+        handleNodeClick(node)
+    }
+}
+
+document.addEventListener("click", interceptClickEvent)
